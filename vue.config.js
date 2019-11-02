f27b551f54e6971ba54b2a507d274bb9ad70543a
clement.dubois <cdubois@octo.com>
2019-11-02 10:31:48 +0100

    STEP 4 Inject Manifest mode

--- vue.config.js
+++ vue.config.js
@@ -1,36 +1,12 @@
-const WorkboxPlugin = require('workbox-webpack-plugin')
-const OCTO_VHS_API = 'https://api-octovhs.herokuapp.com'
-const urlAPIToCache = `(${OCTO_VHS_API}/(.)*/categories/(.)*/videos/)`
+const { InjectManifest } = require('workbox-webpack-plugin')
 
 module.exports = {
   configureWebpack: {
     plugins: [
-      new WorkboxPlugin.GenerateSW({
-        swDest: 'service-worker.js',
-        clientsClaim: true,
-        skipWaiting: true,
-        runtimeCaching: [
-          {
-            urlPattern: new RegExp(`^${urlAPIToCache}`),
-            handler: 'NetworkFirst',
-            options: {
-              cacheableResponse: {
-                statuses: [0, 200]
-              }
-            }
-          },
-          {
-            urlPattern: new RegExp(`^(${OCTO_VHS_API}/categories)`),
-            handler: 'CacheFirst',
-            options: {
-              cacheableResponse: {
-                statuses: [0, 200]
-              }
-            }
-          }
-        ]
-      }
-      )
+      new InjectManifest({
+        swSrc: './src/sw.js',
+        swDest: 'service-worker.js'
+      })
     ]
   }
 }
